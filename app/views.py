0f8ddea749be0839f871857c05c9994cb535ab84
Priyadarshita <gsprout74@gmail.com>
2024-01-13 18:44:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def bs1(request):
+    return render(request,'bs1.html')
+
+def bs2(request):
+    return render(request,'bs2.html')
